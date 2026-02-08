025c279838a63a2f505d85b55b602e9aeeb61f47
Nader Abdelshahid <47378552+nnabdelshahid@users.noreply.github.com>
2026-02-07 17:23:21 -0800

    Add Logger class

---
+++ src/core/Logger.ts
@@ -0,0 +1,11 @@
+export class Logger {
+  log(message: string): void {
+    console.log(`[INFO] ${message}`);
+  }
+  warn(message: string): void {
+    console.warn(`[WARN] ${message}`);
+  }
+  error(message: string): void {
+    console.error(`[ERROR] ${message}`);
+  }
+}
